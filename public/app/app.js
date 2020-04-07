ba597909520ac4f3a0a32b6806e3977db9de4c11
Drewqilong <drewqilong@live.com>
2020-04-06 21:13:28 -0500

    Update

---
+++ public/app/app.js
@@ -0,0 +1,5 @@
+angular.module('userApp', ['appRoutes'])
+
+// .config(function($httpProvider) {
+//     $httpProvider.interceptors.push('AuthInterceptors');
+// });
---
+++ public/app/routes.js
@@ -0,0 +1,190 @@
+var app = angular.module('appRoutes', ['ngRoute'])
+
+// Configure Routes; 'authenticated = true' means the user must be logged in to access the route
+.config(function($routeProvider, $locationProvider) {
+
+    // AngularJS Route Handler
+    $routeProvider
+
+    // Route: Home             
+        .when('/', {
+        templateUrl: 'app/views/pages/home.html'
+    })
+
+    // Route: About Us (for testing purposes)
+    .when('/services', {
+        templateUrl: 'app/views/pages/services.html'
+    })
+
+    // Route: User Registration
+    .when('/register', {
+        templateUrl: 'app/views/pages/users/register.html',
+        controller: 'regCtrl',
+        controllerAs: 'register',
+        authenticated: false
+    })
+
+    // Route: User Login
+    .when('/login', {
+        templateUrl: 'app/views/pages/users/login.html',
+        authenticated: false
+    })
+
+    // Route: User Profile
+    .when('/profile', {
+        templateUrl: 'app/views/pages/users/profile.html',
+        authenticated: true
+    })
+
+    // Route: Facebook Callback Result            
+    .when('/facebook/:token', {
+        templateUrl: 'app/views/pages/users/social/social.html',
+        controller: 'facebookCtrl',
+        controllerAs: 'facebook',
+        authenticated: false
+    })
+
+    // Route: Twitter Callback Result
+    .when('/twitter/:token', {
+        templateUrl: 'app/views/pages/users/social/social.html',
+        controller: 'twitterCtrl',
+        controllerAs: 'twitter',
+        authenticated: false
+    })
+
+    // Route: Google Callback Result
+    .when('/google/:token', {
+        templateUrl: 'app/views/pages/users/social/social.html',
+        controller: 'twitterCtrl',
+        controllerAs: 'twitter',
+        authenticated: false
+    })
+
+    // Route: Facebook Error
+    .when('/facebookerror', {
+        templateUrl: 'app/views/pages/users/login.html',
+        controller: 'facebookCtrl',
+        controllerAs: 'facebook',
+        authenticated: false
+    })
+
+    // Route: Twitter Error
+    .when('/twittererror', {
+        templateUrl: 'app/views/pages/users/login.html',
+        controller: 'twitterCtrl',
+        controllerAs: 'twitter',
+        authenticated: false
+    })
+
+    // Route: Google Error
+    .when('/googleerror', {
+        templateUrl: 'app/views/pages/users/login.html',
+        controller: 'googleCtrl',
+        controllerAs: 'google',
+        authenticated: false
+    })
+
+    // Route: Facebook Account-Inactive Error
+    .when('/facebook/inactive/error', {
+        templateUrl: 'app/views/pages/users/login.html',
+        controller: 'facebookCtrl',
+        controllerAs: 'facebook',
+        authenticated: false
+    })
+
+    // Route: Google Account-Inactive Error
+    .when('/google/inactive/error', {
+        templateUrl: 'app/views/pages/users/login.html',
+        controller: 'googleCtrl',
+        controllerAs: 'google',
+        authenticated: false
+    })
+
+    // Route: Twitter Account-Inactive Error
+    .when('/twitter/inactive/error', {
+        templateUrl: 'app/views/pages/users/login.html',
+        controller: 'twitterCtrl',
+        controllerAs: 'twitter',
+        authenticated: false
+    })
+
+    // Route: Twitter Not Yet Activated Error
+    .when('/twitter/unconfirmed/error', {
+        templateUrl: 'app/views/pages/users/login.html',
+        controller: 'twitterCtrl',
+        controllerAs: 'twitter',
+        authenticated: false
+    })
+
+    // Route: Activate Account Through E-mail
+    .when('/activate/:token', {
+        templateUrl: 'app/views/pages/users/activation/activate.html',
+        controller: 'emailCtrl',
+        controllerAs: 'email',
+        authenticated: false
+    })
+
+    // Route: Request New Activation Link            
+    .when('/resend', {
+        templateUrl: 'app/views/pages/users/activation/resend.html',
+        controller: 'resendCtrl',
+        controllerAs: 'resend',
+        authenticated: false
+    })
+
+    // Route: Send Username to E-mail
+    .when('/resetusername', {
+        templateUrl: 'app/views/pages/users/reset/username.html',
+        controller: 'usernameCtrl',
+        controllerAs: 'username',
+        authenticated: false
+    })
+
+    // Route: Send Password Reset Link to User's E-mail
+    .when('/resetpassword', {
+        templateUrl: 'app/views/pages/users/reset/password.html',
+        controller: 'passwordCtrl',
+        controllerAs: 'password',
+        authenticated: false
+    })
+
+    // Route: User Enter New Password & Confirm
+    .when('/reset/:token', {
+        templateUrl: 'app/views/pages/users/reset/newpassword.html',
+        controller: 'resetCtrl',
+        controllerAs: 'reset',
+        authenticated: false
+    })
+
+    // Route: Manage User Accounts
+    .when('/management', {
+        templateUrl: 'app/views/pages/management/management.html',
+        controller: 'managementCtrl',
+        controllerAs: 'management',
+        authenticated: true,
+        permission: ['admin', 'moderator']
+    })
+
+    // Route: Edit a User
+    .when('/edit/:id', {
+        templateUrl: 'app/views/pages/management/edit.html',
+        controller: 'editCtrl',
+        controllerAs: 'edit',
+        authenticated: true,
+        permission: ['admin', 'moderator']
+    })
+
+    // Route: Search Database Users
+    .when('/search', {
+        templateUrl: 'app/views/pages/management/search.html',
+        controller: 'managementCtrl',
+        controllerAs: 'management',
+        authenticated: true,
+        permission: ['admin', 'moderator']
+    })
+
+    .otherwise({ redirectTo: '/' }); // If user tries to access any other route, redirect to home page
+
+    $locationProvider.html5Mode({ enabled: true, requireBase: false }); // Required to remove AngularJS hash from URL (no base is required in index file)
+});
+
